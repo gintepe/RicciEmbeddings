6ded3a716d6b37c9fe04651f82eda21a7846518a
gintepe <ginte.p@gmail.com>
2020-01-29 12:52:26 +0000

    no more manual saves pls

--- src/test_looper.py
+++ src/test_looper.py
@@ -2,20 +2,27 @@ import numpy as np
 from param_parser import parameter_parser
 from embedding_clustering import create_and_run_model
 import sys
+import time
 
-def report(res):
-    print('average modularity {}'.format(np.mean(res)))
-    print('standard deviation {}'.format(np.var(res)))
-    print(res)
+models = ['DeepWalk', 'DeepWalkWithRegularization', 'Ricci', 'GEMSECRicci', 'GEMSEC', 'GEMSECWithRegularization']
+
+def get_report(res):
+    report = ['average modularity {}'.format(np.mean(res)), 'standard deviation {}'.format(np.var(res))]
+    return '\n'.join(report)
 
 
 if __name__ == "__main__":
     args = parameter_parser()
-    modularities = []
-    for i in range(50):
-        print('trial {}'.format(i + 1))
-        mod = create_and_run_model(args)
-        modularities.append(mod)
-    report(np.asarray(modularities))
+    filename = args.input.split('/')[-1][:-4]
+    with open("./res/{}{}.txt".format(filename, time.time()), "w") as file:
+        for m in models:
+            args.model = m
+            modularities = []
+            for i in range(10):
+                print('trial {}'.format(i + 1))
+                mod = create_and_run_model(args)
+                modularities.append(mod)
+            file.write('{}\n'.format(get_report(np.asarray(modularities))))
+            file.write(str(modularities))
 
     
